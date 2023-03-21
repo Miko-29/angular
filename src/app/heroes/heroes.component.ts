4ca4c39fa90363b4f6eb503fcc98bde0e6fb815f
Miko-29 <muskan2912002@gmail.com>
2023-03-22 03:06:57 +0530

    Add navigation with routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,17 +9,14 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  constructor(private heroService: HeroService, private messageService : MessageService) {}
+  constructor(private heroService: HeroService) {}
   heroes : Hero[] = [];
   selectedHero? : Hero;
 
   ngOnInit(): void {
     this.getHeroes();
   }
-  onSelect(hero:Hero):void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+ 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes);
   }
